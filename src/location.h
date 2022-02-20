66f85c0e57e7fc65ca6d5457b94e24783c670925
Dorian Clay <thedorianmonster@gmail.com>
2022-02-19 18:43:19 -0800

    Make initial files buildable.

--- src/location.h
+++ src/location.h
@@ -1,6 +1,21 @@
 #ifndef LOCATION_H
 #define LOCATION_H
 
+class Location
+{
+protected:
+    int _current[2];
+    Location();
 
+public:
+    Location(int x, int y);
+
+    const int* location() const;
+
+    void move(int x, int y);
+
+    bool operator==(const Location& rhs) const;
+    bool operator!=(const Location& rhs) const;
+};
 
 #endif /* LOCATION_H */
