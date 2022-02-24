5bd1362f3b6ba0e9900a9d6c6bcee82bef745053
Dorian Clay <thedorianmonster@gmail.com>
2022-02-24 14:35:04 -0800

    Create base object class

---
+++ src/game_object.h
@@ -0,0 +1,22 @@
+#ifndef GAME_OBJECT_H
+#define GAME_OBJECT_H
+
+#include "location.h"
+
+class GameObject
+{
+  protected:
+    Location *_location;
+    GameObject()
+    {
+    }
+
+  public:
+    ~GameObject()
+    {
+        delete _location;
+    }
+    Location *location() const { return _location; }
+};
+
+#endif /* GAME_OBJECT_H */
