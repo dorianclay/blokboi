3613afcf3ab55600e7e322ef126f8370d4367df6
Dorian Clay <thedorianmonster@gmail.com>
2022-02-24 13:46:31 -0800

    Use new classes

--- src/map.h
+++ src/map.h
@@ -1,10 +1,23 @@
 #ifndef MAP_H
 #define MAP_H
 
+#include <vector>
+#include "block.h"
+#include "player.h"
+
+typedef std::vector<Block *> Blocks;
+
 class Map
 {
   protected:
+    Blocks _blocks;
+    Blocks _ground;
+    Player _player;
+    unsigned size[2];
+
   public:
+    void generate();
+    void refresh();
 };
 
 #endif /* MAP_H */
