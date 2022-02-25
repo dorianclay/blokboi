82ba67c5f99c1ae04ae1595e4b32b3325ef92133
Dorian Clay <thedorianmonster@gmail.com>
2022-02-25 02:34:13 -0800

    Work on lower classes

---
+++ src/block.py
@@ -0,0 +1,16 @@
+from enum import Enum
+from src.game_object import GameObject
+
+class Color(Enum):
+    RED = 1
+    ORANGE = 2
+    YELLOW = 3
+    GREEN = 4
+    BLUE = 5
+    PURPLE = 6
+    GREY = 7 
+
+class Block(GameObject):
+
+    def __init__(self):
+        self.color = Color.GREY
---
+++ src/game_object.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+class GameObject(object):
+
+    def __init__(self):
+        self.location = np.array([0, 0])
