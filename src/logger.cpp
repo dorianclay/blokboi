fe90be3ca803b01ed112c49c71b2ff32aded6d31
Dorian Clay <thedorianmonster@gmail.com>
2022-02-25 12:33:28 -0800

    Use logger

---
+++ src/logger.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+void log(const string &str, const string &arg = "")
+{
+    char buf[1000];
+
+    snprintf(buf, sizeof(buf), str.c_str(), arg.c_str());
+    cout << buf << endl;
+}
+
+void logerror(const string &str, const string &arg = "")
+{
+    char buf[1000];
+
+    snprintf(buf, sizeof(buf), str.c_str(), arg.c_str());
+    cerr << buf << endl;
+}
