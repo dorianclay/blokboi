05e4233e60e7a5d24f0f8333122d9eb040deb4d5
Dorian Clay <dorian@dorianclay.com>
2022-02-28 13:43:10 -0800

    Add more prints

--- src/game.cpp
+++ src/game.cpp
@@ -6,6 +6,6 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << endl;
-    // Scene earth = Scene(20, 30);
+    cout << "Hello World!" << endl << "I'm making a scene..." << endl;
+    Scene earth = Scene(20, 30);
 }
