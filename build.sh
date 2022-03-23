88cbfa79e14b078cfeb4ac48e9fe2d0e035116c8
Dorian Clay <thedorianmonster@gmail.com>
2022-03-23 14:43:40 -0700

    Exit on build failure

--- build.sh
+++ build.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
-cmake .
-cmake --build .
-sh cython_build.sh
+SECTION=""
+
+die() {
+    printf "\033[31mFailed on $SECTION step.\n\033[0m"
+    exit 1
+}
+
+SECTION="CMake configuration"
+cmake . || die
+
+SECTION="C++ build"
+cmake --build . || die
+
+SECTION="Cython build"
+sh cython_build.sh || die
+
+printf "\033[32mBuilt successfully.\n\033[0m"
+exit 0
