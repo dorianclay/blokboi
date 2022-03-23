4121e9558b75227278be8ba48eca4072f62d5076
Dorian Clay <thedorianmonster@gmail.com>
2022-03-23 00:44:23 -0700

    Clean up code

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import argparse
+# import argparse
 
 from src.logger import Logger
 from src.pycolors import Colors
@@ -7,7 +7,11 @@ from lib.cgame import PyGame
 def main():
     logger = Logger.log_setup('blokboi', detail='debug', suppress_datetime=False, console=False)
 
-    print(Colors.makeCyan('Hello World!'))
+    print(Colors.makeCyan('Hello World! Starting blokboi slave'))
+
+    game_instance = PyGame()
+    game_instance.newGame()
+    logger.info(game_instance.representation())
 
 if __name__ == '__main__':
     main()
