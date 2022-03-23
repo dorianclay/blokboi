278e77f4218428d5473006545480784f3ca77a23
Dorian Clay <thedorianmonster@gmail.com>
2022-03-23 00:34:55 -0700

    Use Cython by macro for release

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
-from distutils.core import setup
-from Cython.Build import cythonize
+from numpy import True_
+from setuptools import Extension, setup
+
+USE_CYTHON = True
+
+ext = ".pyx" if USE_CYTHON else ".c"
+
+extensions = [Extension("cgame", ["lib/cgame"+ext])]
+
+if USE_CYTHON:
+    from Cython.Build import cythonize
+    extensions = cythonize(extensions, compiler_directives={'language_level':3})
 
 setup(
     name="blokboi",
-    ext_modules=cythonize('lib/*.pyx',
-                          compiler_directives={
-                              'language_level': 3,
-                          },
-                        #   include_path=['include/loguru',
-                        #                 'include/random'
-                        #   ]
-    ),
+    ext_modules=extensions
 )
-
-# 'src/*.pyx', 
