1423ec5392f8b080306d3373c283fdcf1dfcd668
Dorian Clay <thedorianmonster@gmail.com>
2022-03-24 02:26:28 -0700

    Clean build scripts

--- build.sh
+++ build.sh
@@ -9,11 +9,9 @@ die() {
 
 if [[ $1 ]]; then
     if [ $1 = "clean" ]; then
-        SECTION="Cython clean"
-        python3 setup.py clean --all || die
         SECTION="make clean"
         make clean || die
-        printf "\033[33mSuccessfully cleaned cython and c++ build files.\n\033[0m"
+        printf "\033[33mSuccessfully cleaned C++ build files.\n\033[0m"
     else
         printf "Current options are:\n"
         printf "\tclean\tRemove all build files.\n"
@@ -24,9 +22,6 @@ else
 
     SECTION="C++ build"
     cmake --build . || die
-
-    SECTION="Cython build"
-    sh cython_build.sh || die
 
     printf "\033[32mBuilt successfully.\n\033[0m"
 fi
