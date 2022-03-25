93cd76859dbdb1bcb0c8d4d682a0a2ad4067eb8c
Dorian Clay <thedorianmonster@gmail.com>
2022-03-25 14:27:33 -0700

    Get images to print

--- src/image_gen.py
+++ src/image_gen.py
@@ -10,6 +10,34 @@ for path in paths:
 
 class ImageGen:
 
+    _asset_map = {
+        '.': 'sky',
+        '@': 'ground',
+        'r': 'block_red_x',
+        'o': 'block_orange_x',
+        'y': 'block_yellow_x',
+        'g': 'block_green_x',
+        'b': 'block_blue_x',
+        'p': 'block_purple_x',
+        'P': 'boi_2'
+    }
+
+    _skycolor = (90,192,236)
+
     def make_image_from(mapstring: str, path: Path) -> None:
         logger = logging.getLogger('image_gen')
-        pass
+        lines = mapstring.split('\n')
+        width = len(lines[0]) * 16
+        height = len(lines) * 16
+        newImage = Image.new('RGB', (width, height), ImageGen._skycolor)
+        count_r = 0
+        for row in lines:
+            count_c = 0
+            for col in row:
+                region = assets[ImageGen._asset_map[col]].crop((0,0,16,16))
+                newImage.paste(region, 
+                                (count_c*16, count_r*16, (count_c+1)*16, (count_r+1)*16),
+                                mask=region)
+                count_c += 1
+            count_r += 1
+        newImage.save(path)
