41883c615c0a3a07e286737a6688e3f8a92b38db
Dorian Clay <thedorianmonster@gmail.com>
2022-03-25 14:27:33 -0700

    Get images to print

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # import argparse
-
+from pathlib import Path
 from src.logger import Logger
 from src.pycolors import Colors
+from src.image_gen import ImageGen
 from blokboi import Game
 
 def main():
@@ -11,6 +12,8 @@ def main():
 
     game_instance = Game()
     logger.info("Got map:\n" + game_instance.__repr__())
+
+    ImageGen.make_image_from(game_instance.__repr__(), Path('test.png'))
 
 if __name__ == '__main__':
     main()
