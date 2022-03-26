31962c0e989430a1db1f7edc6d60fa15a36b7289
Clang Format <clang-format@1337z.ninja>
2022-03-26 00:30:30 +0000

    apply clang-format

--- lib/cgame_object.cpp
+++ lib/cgame_object.cpp
@@ -13,17 +13,14 @@ PYBIND11_MODULE(game_object, m)
     py::class_<location>(m, "location")
         .def_readonly("x", &location::x)
         .def_readonly("y", &location::y)
-        .def("__repr__", [](location &a) {
-            return "<location (" + to_string(a.x) + ", " + to_string(a.y) + ")>";
-        });
+        .def("__repr__", [](location &a) { return "<location (" + to_string(a.x) + ", " + to_string(a.y) + ")>"; });
 
     py::class_<GameObject>(m, "GameObject")
         // .def("kind", &Block::kind)
         // .def("update", &Block::update)
         .def("location", &Block::location)
         .def("movable", &Block::movable)
-        .def("__repr__", [](GameObject &a)
-        {
+        .def("__repr__", [](GameObject &a) {
             return "<game_object at (" + to_string(a.location().x) + ", " + to_string(a.location().y) + ")>";
         });
 
@@ -33,14 +30,12 @@ PYBIND11_MODULE(game_object, m)
         // .def(py::init<int, int, Color, int, bool>())
         .def("color", &Block::color)
         .def("number", &Block::number)
-        .def("__repr__", [](Block &a) { 
-            return "<game_object.Block at (" + to_string(a.location().x) + ", " + to_string(a.location().y) + ") with color " + to_string(a.color()) + " and number " + to_string(a.number()) + ">"; 
+        .def("__repr__", [](Block &a) {
+            return "<game_object.Block at (" + to_string(a.location().x) + ", " + to_string(a.location().y) +
+                   ") with color " + to_string(a.color()) + " and number " + to_string(a.number()) + ">";
         });
 
-    py::class_<Ground, GameObject>(m, "Ground")
-        .def(py::init<int, int>())
-        .def("__repr__", [](Ground &a)
-        {
-            return "<game_object.Ground at (" + to_string(a.location().x) + ", " + to_string(a.location().y) + ")>";
-        });
+    py::class_<Ground, GameObject>(m, "Ground").def(py::init<int, int>()).def("__repr__", [](Ground &a) {
+        return "<game_object.Ground at (" + to_string(a.location().x) + ", " + to_string(a.location().y) + ")>";
+    });
 }
