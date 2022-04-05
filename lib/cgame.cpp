ac6f12eacaa961fe37dd78e81a21a00df222e6a7
Dorian Clay <thedorianmonster@gmail.com>
2022-04-05 15:02:14 -0700

    Don't use buffer protocol to expose array.

--- lib/cgame.cpp
+++ lib/cgame.cpp
@@ -9,7 +9,7 @@ PYBIND11_MODULE(blokboi, m)
 {
     m.doc() = "Blokboi game";
 
-    py::class_<Game>(m, "Game", py::buffer_protocol())
+    py::class_<Game>(m, "Game")
         .def(py::init())
         .def("newGame", &Game::newGame)
         .def("resetGame", &Game::resetGame)
@@ -19,22 +19,8 @@ PYBIND11_MODULE(blokboi, m)
         .def("put_down", &Game::put_down)
         .def("width", &Game::width)
         .def("height", &Game::height)
-        .def("data", &Game::data)
         .def("array", &Game::array)
-        // // TODO: Make the <vector<vector: GameObject>> an opaque type
-        // .def("scene_space", &Game::scene_space)
-        // // TODO (END)
         .def("representation", &Game::representation)
         .def("objective", &Game::objective)
-        .def("__repr__", [](Game &a) { return a.representation(); })
-        .def_buffer([](Game &m) -> py::buffer_info {
-            return py::buffer_info(
-                m.data(),
-                sizeof(char),
-                py::format_descriptor<char>::format(),
-                3,
-                { m.width(), m.height(), 2 },
-                { sizeof(char) * m.height() * 2, sizeof(char) * 2, sizeof(char)}
-            );
-        });
+        .def("__repr__", [](Game &a) { return a.representation(); });
 }
