51c0732d827b151875da867cdd7168f1cd7d0b48
Dorian Clay <thedorianmonster@gmail.com>
2022-04-08 17:49:45 -0700

    Fill canvas with scene!

--- src/gui.py
+++ src/gui.py
@@ -1,117 +1,106 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import Image, ttk
 from pathlib import Path
 from PIL import ImageTk
 import logging
 
 from src.image_gen import ImageGen
+from blokboi import Game
 
 
 class CanvasFrame(ttk.Frame):
     def __init__(self, container):
         super().__init__(container)
-        self._scale = container._scale
+        self._container = container
+        self.logger = logging.getLogger("blokboi.app.canvas")
+        self.logger.debug("Initializing canvas to draw on.")
 
         # Initialize canvas
+        scale = self._container._scale
         self.canvas = tk.Canvas(
             self,
             bg=f"#{ImageGen.get_sky_hex()}",
-            width=container._scenewidth * 16 * self._scale,
-            height=container._sceneheight * 16 * self._scale,
+            width=container._scenewidth * 16 * scale,
+            height=container._sceneheight * 16 * scale,
         )
         self.canvas.pack()
         self.pack()
 
         # Set up the dictionary of images
-        paths = (container._assetpath / f"{self._scale}").glob(f"**/*.png")
+        paths = (container._assetpath / f"{scale}x").glob(f"**/*.png")
         self._assets = {}
         for path in paths:
             self._assets[path] = ImageTk.PhotoImage(file=str(path))
 
+        self.render()
+
+    def render(self):
+        width = self._container._scenewidth
+        height = self._container._sceneheight
+        assetpath = self._container._assetpath
+        scale = self._container._scale
+        self.canvas.delete("all")
+        for row in range(width):
+            for col in range(height):
+                self._draw(
+                    row,
+                    col,
+                    self._assets[
+                        ImageGen.get_image_path(
+                            self._container._game_instance.array()[row][col],
+                            scale,
+                            assetpath,
+                        )
+                    ],
+                )
+
     def _draw(self, row, col, image):
-        block = 16 * self._scale
+        block = 16 * self._container._scale
         self.canvas.create_image(
-            (block / 2) + block * row, (block / 2) + block * col, image=image
+            # x
+            (block / 2) + block * row,
+            # y
+            (block / 2) + block * (self._container._sceneheight - 1 - col) + 1,
+            # ImageTk.PhotoImage object
+            image=image,
         )
 
 
 class ButtonFrame(ttk.Frame):
     def __init__(self, container):
         super().__init__(container)
+        self.logger = logging.getLogger("blokboi.app.button")
+        self._container = container
 
         self.rowconfigure(0, weight=1)
 
         self.__create_widgets()
 
     def __create_widgets(self):
-        ttk.Button(self, text="reset").grid(column=0, row=0)
-        ttk.Button(self, text="new scene").grid(column=1, row=0)
+        ttk.Button(self, text="reset", command=self.reset_clicked).grid(column=0, row=0)
+        ttk.Button(self, text="new scene", command=self.new_scene_clicked).grid(
+            column=1, row=0
+        )
 
         for widget in self.winfo_children():
-            widget.grid(padx=8, pady=0)
+            widget.grid(padx=16, pady=0)
 
+    def reset_clicked(self):
+        self.logger.debug("Reset clicked")
+        self._container._game_instance.resetGame()
+        self._container.render()
 
-class SceneFrame(ttk.Frame):
-    def __init__(self, container, game_instance, assetpath: Path = Path("assets")):
-        super().__init__(container)
-        self.logger = logging.getLogger("blokboi.app.sceneframe")
-        self.logger.info("Starting GUI scene frame.")
-
-        options = {"padx": 0, "pady": 0}
-
-        width = game_instance.width()
-        height = game_instance.height()
-
-        for col in range(width):
-            self.columnconfigure(col, weight=1)
-
-        for row in range(height):
-            self.rowconfigure(row, weight=1)
-
-        row = 0
-        col = 0
-        path = ImageGen.get_image_path(
-            game_instance.array()[row][col],
-            container._scale,
-            assetpath=assetpath,
-        )
-        self.logger.info(f"Path: {path}")
-        photo = tk.PhotoImage(file=str(path))
-        self.image_labelx = ttk.Label(
-            self,
-            image=photo,
-            padding=0,
-        )
-        self.image_labelx.pack(**options)
-
-        label = ttk.Label(self, text="Hello, owrkd!")
-        label.pack(**options)
-        self.pack(**options)
-
-        # for row in range(width):
-        #     for col in range(height):
-        #         # image_labelx = ttk.Label(
-        #         #     container,
-        #         #     image=tk.PhotoImage(str(ImageGen.get_image_path(
-        #         #         game_instance.array()[row][col],
-        #         #         container.scale,
-        #         #         assetpath=assetpath,
-        #         #     ))),
-        #         #     padding=0,
-        #         # )
-        #         # image_labelx.grid(column=col, row=row)
-        #         self.label=ttk.Label(self, text='t')
-        #         self.label.grid(column=col, row=row)
-
-    def arrow_clicked(self):
-        self.showinfo(title="Success", message="An arrow was clicked!")
+    def new_scene_clicked(self):
+        self.logger.debug("New Scene clicked")
+        self._container._game_instance.newGame()
+        self._container.render()
 
 
 class App(tk.Tk):
     def __init__(self, width=20, height=15, *, scale=2, assetpath=Path("assets")):
         super().__init__()
         self.logger = logging.getLogger("blokboi.app")
-        self.logger.info("Starting GUI app.")
+        self.logger.info("Starting Blokboi app...")
 
         self._scale = scale
         self._scenewidth = width
@@ -119,11 +108,13 @@ class App(tk.Tk):
         self._assetpath = assetpath
 
         self.title("Blokboi")
-        self.__center(width * 16 * scale, height * 16 * scale + 8)
+        self.__center(width * 16 * scale, height * 16 * scale + 48)
         self.resizable(False, False)
 
         self.rowconfigure(0, weight=2 * height)
         self.rowconfigure(0, weight=1)
+
+        self._game_instance = Game()
 
         self.__create_widgets()
 
@@ -139,11 +130,14 @@ class App(tk.Tk):
         self.geometry(f"{width}x{height}+{center_x}+{center_y}")
 
     def __create_widgets(self):
-        canvas_frame = CanvasFrame(self)
-        canvas_frame.grid(row=0)
+        self.canvas_frame = CanvasFrame(self)
+        self.canvas_frame.grid(row=0, sticky=tk.N)
 
-        button_frame = ButtonFrame(self)
-        button_frame.grid(row=1)
+        self.button_frame = ButtonFrame(self)
+        self.button_frame.grid(row=1)
+
+    def render(self):
+        self.canvas_frame.render()
 
 
 if __name__ == "__main__":
