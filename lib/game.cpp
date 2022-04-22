5111c28417ef27b4ca73fd7b0764b7c6a68deeba
Dorian Clay <thedorianmonster@gmail.com>
2022-04-22 16:52:16 -0700

    Place blocks more intelligently

--- lib/game.cpp
+++ lib/game.cpp
@@ -16,7 +16,7 @@ Game::Game() {
   LOG_F(INFO, "Beginning a new game.");
 
   _scene = new Scene(20, 15);
-  _scene->generate_heuristical();
+  _scene->generate();
   _player_controller = new PlayerController(_scene, _scene->get_player());
   DLOG_F(INFO, "Map generated:\n%s", _scene->representation().c_str());
 }
@@ -40,7 +40,7 @@ void Game::newGame() {
   delete _scene;
   delete _player_controller;
   _scene = new Scene(20, 15);
-  _scene->generate_modular();
+  _scene->generate();
   _player_controller = new PlayerController(_scene, _scene->get_player());
   DLOG_F(INFO, "New map generated:\n%s", _scene->representation().c_str());
 }
